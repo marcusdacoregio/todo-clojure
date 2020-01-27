df148b155919f625b619b35831b213349de07d6a
Marcus da Corégio <marcusdacoregio@gmail.com>
2020-01-27 11:46:36 -0200

    initial commit

---
+++ src/todo/query.clj
@@ -0,0 +1,22 @@
+(ns todo.query
+  (:require [todo.database]
+            [korma.core :refer :all]))
+
+(defentity items)
+
+(defn get-todos [] (select items))
+
+(defn add-todo [title description]
+  (insert items (values {:title title :description description})))
+
+(defn delete-todo [id]
+  (delete items (where {:id [= id]})))
+
+(defn update-todo [id title is-complete]
+  (update items
+          (set-fields {:title title
+                       :is-complete is-complete})
+          (where {:id [= id]})))
+
+(defn get-todo [id]
+  (first (select items (where {id [= id]}))))
